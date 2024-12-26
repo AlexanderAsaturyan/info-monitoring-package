1ffebcba422145641e17264d365c6eaa3c962618
Alexander Asaturyan <>
2024-12-26 16:15:54 +0400

    Changed field Category into Property, added BatteryData class.

--- BaseData/BaseData.cs
+++ BaseData/BaseData.cs
@@ -2,6 +2,15 @@ namespace DefaultNamespace
 {
     public class BaseData
     {
-        public virtual string Category;
+        public virtual string Category { get; set;}
+    }
+
+    public class BatteryData : BaseData
+    {
+        public override string Category = "Battery";
+        public int Level;
+        public string Type;
+        public string State;
+        public int DecayRate;
     }
 }
